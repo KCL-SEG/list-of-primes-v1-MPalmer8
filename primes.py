c6413d5b8e716746c63914acfc921c5a079117e6
Matthew Palmer <matthewpalmer135@gmail.com>
2022-10-04 14:46:39 +0100

    Add primes functionality

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def numIsPrime(n):
+    for num in range(2,upper):
+        if upper%num == 0:
+            return False
+    return True
+
+
 def primes(number_of_primes):
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if numIsPrime:
+            list.append(num)
+        num += 1
     return list
+
+
+    print(primes(6))
